a95d7ced40ae80f14edfb32d26fb1f3b3774edfb
aymeric75 <aymeric.barbin@hotmail.fr>
2025-06-12 11:52:13 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from "next"
 import { Button } from "components/Button/Button"
 import HeroVideo from 'components/HeroVideo'
+import HowItWorks from 'components/HowItWorks'
 import { LP_GRID_ITEMS } from "lp-items"
 
 export const metadata: Metadata = {
@@ -24,6 +25,7 @@ export default function Web() {
   return (
     <>
       <HeroVideo />
+      <HowItWorks />
       <section className="bg-white dark:bg-gray-900">
         <div className="mx-auto grid max-w-(--breakpoint-xl) px-4 py-8 text-center lg:py-16">
           <div className="mx-auto place-self-center">
