541e9d25dcb5302f5a24e9881d3b4d6bb6e5b7bb
aymeric75 <aymeric.barbin@hotmail.fr>
2025-06-12 11:12:07 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -23,6 +23,7 @@ export const metadata: Metadata = {
 export default function Web() {
   return (
     <>
+      <HeroVideo />
       <section className="bg-white dark:bg-gray-900">
         <div className="mx-auto grid max-w-(--breakpoint-xl) px-4 py-8 text-center lg:py-16">
           <div className="mx-auto place-self-center">
